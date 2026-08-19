fe9105c43ebef9ab31154da446e19b0ae697c32d
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2026-08-19 17:54:04 -0300

    primeiro exercicio

---
+++ lista1/bloco1/bloco_a.dart
@@ -0,0 +1,17 @@
+// exercicio A1
+
+void a1(){
+    String nome = 'Luiz'; // nome produtor
+
+    String cidade = 'Ceres';
+
+    double area = 5;
+
+    int ano = 2026;
+
+    print('O produtor $nome da cidade $cidade Possui $area hectares no ano de $ano');
+}
+
+void main(){
+    a1();
+}
