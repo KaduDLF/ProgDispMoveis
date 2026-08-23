297a8b9b54ce3cf7b05144beeeb93fb5211bb167
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2026-08-23 17:34:44 -0300

    exercicio 2 bloco a

--- lista1/bloco1/bloco_a.dart
+++ lista1/bloco1/bloco_a.dart
@@ -12,6 +12,18 @@ void a1(){
     print('O produtor $nome da cidade $cidade Possui $area hectares no ano de $ano');
 }
 
+void a2(){
+    var nome = 'Luiz';
+
+    var cidade = 'Ceres';
+
+    var area = 5.00;
+
+    var ano = 2026;
+
+    print('O produtor $nome da cidade $cidade Possui $area hectares no ano de $ano');
+}
+
 void main(){
-    a1();
+    a2();
 }
